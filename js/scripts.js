42e66dac42e53b3b4d50fc44204e795ad84438c0
James Lannon <jamesmlannon1990@live.com>
2017-06-18 18:24:12 -0700

    style work section, add javascript to display project-text over project-img, change process section images to background images

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+  $("div#project-1-card").hover(function(){
+    $("div#project-1-text").fadeIn('slow');
+  },
+  function(){
+    $("div#project-1-text").fadeOut('slow');
+  });
+
+  $("div#project-2-card").hover(function(){
+    $("div#project-2-text").fadeIn('slow');
+  },
+  function(){
+    $("div#project-2-text").fadeOut('slow');
+  });
+
+  $("div#project-3-card").hover(function(){
+    $("div#project-3-text").fadeIn('slow');
+  },
+  function(){
+    $("div#project-3-text").fadeOut('slow');
+  });
+
+
+});
